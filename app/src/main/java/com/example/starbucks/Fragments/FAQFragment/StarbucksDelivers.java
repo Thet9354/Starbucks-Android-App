732ed291d261065531ca79d82c74659f148a8ee7
PhoonThetPine <thetpine254@gmail.com>
2023-04-29 21:02:51 +0800

    Android Starbucks App - V13

--- app/src/main/java/com/example/starbucks/Fragments/FAQFragment/StarbucksDelivers.java
+++ app/src/main/java/com/example/starbucks/Fragments/FAQFragment/StarbucksDelivers.java
@@ -1,66 +1,59 @@
 package com.example.starbucks.Fragments.FAQFragment;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.example.starbucks.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link StarbucksDelivers#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class StarbucksDelivers extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private LinearLayout ll_starbucksDeliveryOrder, ll_selectLocation, ll_redeemRewardOnline, ll_doINeedToBeAMember,
+            ll_minOrderDelivery, ll_deliveryFee, ll_paymentModesDelivery, ll_multiplePaymentDelivery,
+            ll_cashOnDelivery, ll_cancelledOrder, ll_amendOrders, ll_orderCancelled, ll_refundIfOrderCancelled;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private LinearLayout ll_starbucksDeliveryOrderDetail, ll_selectLocationDetail, ll_scheduleDeliveryTimeDetail,
+            ll_doINeedToBeAMemberDetail, ll_minOrderDeliveryDetails, ll_deliveryFeeDetail, ll_paymentModesDeliveryDetail,
+            ll_multiplePaymentDeliveryDetail, ll_cashOnDeliveryDetail, ll_cancelledOrderDetail, ll_amendOrdersDetail,
+            ll_orderCancelledDetail, ll_refundIfOrderCancelledDetail;
 
-    public StarbucksDelivers() {
-        // Required empty public constructor
-    }
+    private ImageView imgView_starbucksDeliveryOrder, imgView_selectLocation, imgView_scheduleDeliveryTime, imgView_doINeedToBeAMember,
+            imgView_minOrderDelivery, imgView_deliveryFee, imgView_paymentModesDelivery, imgView_multiplePaymentDelivery,
+            imgView_cashOnDelivery, imgView_cancelledOrder, imgView_amendOrders, imgView_orderCancelled, imgView_refundIfOrderCancelled;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment StarbucksDelivers.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static StarbucksDelivers newInstance(String param1, String param2) {
-        StarbucksDelivers fragment = new StarbucksDelivers();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private Context mContext;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_starbucks_delivers, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_starbucks_delivers, container, false);
+
+        mContext = getActivity();
+
+        getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        return rootView;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        findViews(view);
+    }
+
+    private void findViews(View v) {
+
     }
 }
